60530616983db1a908b8ebbf65e8a9b91593af7d
The coolest farley <surelyilikemen@gmail.com>
2025-05-30 20:58:20 -0500

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,179 @@
+local BotCore = {} -- I will add a gui version for bridged connections later on(never 💔)
+
+local Players = game:GetService("Players")
+
+local TextChatService = game:GetService("TextChatService")
+
+local function validate(defaults, args)
+	for i,v in pairs(defaults) do
+		if args[i] == nil then
+			args[i] = v
+		end
+	end   
+	return args
+end
+
+local function FindPlayerByDisplayName(name, owner)
+    if name == "" or name == nil or name == "nil" then
+        return owner
+    end
+
+    for i,v in pairs(Players:GetPlayers()) do
+        local loweredDName = v.DisplayName:lower()
+        local loweredName = v.Name:lower()
+
+        if string.find(loweredDName, name:lower(), 1, true) or string.find(loweredName, name:lower(), 1, true) then
+            return v
+        end
+    end
+end
+
+function BotCore.new(params)
+    local Bot = validate({
+        Owner = 1, -- Main user id.
+
+        Bot = Players.LocalPlayer, -- The bot!
+
+        Prefix = "!", -- What you use to make the commands work eg. !here
+
+        Commands = {}, -- Stores commands duh! will add the basic ones (dictionary). 
+
+        Log = {}, -- Stores all commands that have been used
+
+        Whitelisted = {}, -- Allows these users to use commands at certain ranks and etc (dictionary).
+
+        Excluded = {} -- Doesn't let the bot kill these users wiith kill all and what not.
+    }, params)
+
+    local Owner = Players:GetPlayerByUserId(Bot.Owner)
+
+    -- Main functions
+
+    function Bot:GetUserRank(user : Player)
+        return Bot.Whitelisted[user.UserId].Rank
+    end
+
+    function Bot.CreateCommand(name : string, aliases : table, requiredRank : number, callback)
+        local data = {
+            Name = name, -- The name of the command
+
+            Aliases = aliases, -- The other names of the command
+
+            RequiredRank = requiredRank, -- The rank need to run this command
+
+            Callback = callback -- The function of the command
+        }
+
+        Bot.Commands[name:lower()] = data
+
+        if #aliases > 0 then
+            for _, alias in aliases do
+                Bot.Commands[alias:lower()] = data
+            end
+        end
+    end
+
+    function Bot.AddUser(user : Player, rank)
+        if Bot.Whitelisted[user.UserId] then
+            warn("Already found a user for this user")
+            return
+        end
+
+        Bot.Whitelisted[user.UserId] = {Name = user.Name, Id = user.UserId, Rank = rank}
+
+        user.Chatted:Connect(function(message)
+            if not message:sub(1, #Bot.Prefix) == Bot.Prefix then
+                return
+            end
+
+            local raw = message:sub(#Bot.Prefix + 1)
+            local args = {}
+
+            for word in raw:gmatch("%S+") do
+                table.insert(args, word)
+            end
+
+            local command = args[1]
+            table.remove(args, 1)
+            
+            local commandData = Bot.Commands[command]
+
+            if commandData then
+                if Bot:GetUserRank(user) <= commandData.RequiredRank and Bot.Whitelisted[user.UserId] ~= nil then
+                    commandData.Callback(user, args)
+                    Bot.LogCommand(user, commandData.Name, os.date("%Y-%m-%d %H:%M:%S"), table.concat(args, ", "))
+                else
+                    warn("User (", user.DisplayName, ") does not meet the required rank for this command (", commandData.Name, ")")
+                end
+            end
+        end)
+    end
+
+    function Bot.RemoveUser(user : Player)
+        if Bot.Whitelisted[user.UserId] then
+            Bot.Whitelisted[user.UserId] = nil    
+        end
+    end
+
+    function Bot.AddExclusion(user : Player)
+        Bot.Excluded[user.UserId] = user
+    end
+
+    function Bot.RankUp(user : Player, amount)
+        if Bot.Whitelisted[user.UserId] then
+            Bot.Whitelisted[user.UserId].Rank -= amount
+        end
+    end
+
+    function Bot.DeRank(user : Player, amount : number)
+        if Bot.Whitelisted[user.UserId] then
+            Bot.Whitelisted[user.UserId].Rank += amount
+        end
+    end
+
+    function Bot.LogCommand(sender, name, time, args)
+        Bot.Log[sender.DisplayName.."("..time..")"] = {Name = name, Args = args}
+    end
+
+    -- Connections
+
+    Players.PlayerRemoving:Connect(function(player)
+        if player == Owner then
+            game:Shutdown()
+        end
+    end)
+
+    -- Setup
+
+    Bot.AddUser(Owner, 1)
+
+    Bot.CreateCommand("whitelist", {"wl"}, 1, function(sender, args)
+        Bot.AddUser(FindPlayerByDisplayName(args[1], sender), args[2])
+    end)
+
+    Bot.CreateCommand("blacklist", {"removewhitelist", "bl"}, 1, function(sender, args)
+        Bot.RemoveUser(FindPlayerByDisplayName(args[1], nil))
+    end)
+
+    Bot.CreateCommand("exclude", {"exc"}, 1, function(sender, args)
+        Bot.AddExclusion(FindPlayerByDisplayName(args[1], sender))
+    end)
+
+    Bot.CreateCommand("rankup", {"ru", "addrank"}, 1, function(sender, args)
+        Bot.RankUp(FindPlayerByDisplayName(args[1], nil), args[2])
+    end)
+
+    Bot.CreateCommand("derank", {"dr", "removerank", "rr"}, 1, function(sender, args)
+        Bot.DeRank(FindPlayerByDisplayName(args[1], nil), args[2])
+    end)
+
+    Bot.CreateCommand("log", {"usedcommands", "printlog"}, 5, function(sender, args)
+        for i,v in pairs(Bot.Log) do
+            print(i .. ": command = " .. v.Name .. ", args = " .. v.Args)            
+        end
+    end)
+
+    return Bot
+end
+
+return BotCore
